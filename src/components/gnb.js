8ee4b13be695a368d9f14feb52cd0101db044a32
Yo sup <dhdytjq2@gamil.com>
2023-06-14 22:49:14 +0900

    redeploy

--- src/components/gnb.js
+++ src/components/gnb.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 export default function Gnb() {
   return (
     <div className="w-[80%] p-7  flex justify-around">
-      <Link to="/">
+      <Link to={`${process.env.PUBLIC_URL}/`}>
         <a>Home</a>
       </Link>
-      <Link to="/signin">
+      <Link to={`${process.env.PUBLIC_URL}/signin`}>
         <a>sign in</a>
       </Link>
-      <Link to="/signup">
+      <Link to={`${process.env.PUBLIC_URL}/signup`}>
         <a>sign up</a>
       </Link>
     </div>
