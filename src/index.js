a24deb6aadbc82c75dec02ed708862076a06d567
Yo sup <dhdytjq2@gamil.com>
2023-06-14 22:36:30 +0900

    redeploy

--- src/index.js
+++ src/index.js
@@ -2,29 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import axios from "axios";
 
 axios.interceptors.response.use(
-  function(res){
-    return res
+  function (res) {
+    return res;
   },
-  function(err){
-    if(err.response.status === 401){
-      if(err.response.data.code === '4401'){
-        window.location.href = '/signin'
+  function (err) {
+    if (err.response.status === 401) {
+      if (err.response.data.code === "4401") {
+        window.location.href = "/signin";
       }
     }
     return Promise.reject(err);
   }
-)
-
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
